b97f89a8514c6cfd08b6dd040cd6f2f57962977a
Elianchis <eliantamayo6@gmail.com>
2023-05-17 12:06:14 -0500

    Actualización 3.0

--- src/App.vue
+++ src/App.vue
@@ -1,63 +1,82 @@
 <template>
   <div>
-    <nav class="navbar">
-      <div class="navbar-logo">
+    <div v-if="!isLoggedIn">
+      <div v-if="showRegister" class="register-form">
+        <h2>Registrarse</h2>
+        <input type="text" v-model="registerForm.username" placeholder="Usuario" />
+        <input type="password" v-model="registerForm.password" placeholder="Contraseña" />
+        <button @click="registerUser">Registrarse</button>
+        <button @click="showLoginForm">Iniciar Sesión</button>
+      </div>
+
+      <div v-else class="login-form">
+        <h2>Iniciar Sesión</h2>
+        <input type="text" v-model="loginForm.username" placeholder="Usuario" />
+        <input type="password" v-model="loginForm.password" placeholder="Contraseña" />
+        <button @click="loginUser">Iniciar Sesión</button>
+        <button @click="showRegisterForm">Registrarse</button>
+      </div>
+    </div>
+
+    <div v-else>
+      <nav class="navbar">
+        <div class="navbar-logo">
+          <h1>GESTAPP</h1>
+        </div>
+        <div class="navbar-links">
+          <span class="username">{{ loggedInUser }}</span>
+          <button class="logout-button" @click="logout">Cerrar Sesión</button>
+        </div>
+      </nav>
+
+      <div class="container">
         <h1>GESTAPP</h1>
-      </div>
-      <div class="navbar-links">
-        <a href="#">Inicio</a>
-        <a href="#">Tareas</a>
-        <a href="#">Configuración</a>
-      </div>
-    </nav>
 
-    <div class="container">
-      <h1>GESTAPP</h1>
+        <form @submit.prevent="addTask">
+          <label for="title">Título:</label>
+          <input type="text" id="title" v-model="newTask.title" />
 
-      <form @submit.prevent="addTask">
-        <label for="title">Título:</label>
-        <input type="text" id="title" v-model="newTask.title" />
+          <label for="description">Descripción:</label>
+          <textarea id="description" v-model="newTask.description"></textarea>
 
-        <label for="description">Descripción:</label>
-        <textarea id="description" v-model="newTask.description"></textarea>
+          <button>Agregar Tarea</button>
+        </form>
 
-        <button>Agregar Tarea</button>
-      </form>
+        <div class="task-list">
+          <h2>Tareas Pendientes</h2>
 
-      <div class="task-list">
-        <h2>Tareas Pendientes</h2>
+          <div v-if="pendingTasks.length === 0">No hay tareas pendientes.</div>
 
-        <div v-if="pendingTasks.length === 0">No hay tareas pendientes.</div>
+          <div v-for="task in pendingTasks" :key="task.id" class="task">
+            <div class="task-content">
+              <h3>{{ task.title }}</h3>
+              <p>{{ task.description }}</p>
+            </div>
 
-        <div v-for="task in pendingTasks" :key="task.id" class="task">
-          <div class="task-content">
-            <h3>{{ task.title }}</h3>
-            <p>{{ task.description }}</p>
-          </div>
-
-          <div class="task-actions">
-            <button @click="completeTask(task.id)">Completar</button>
-            <button @click="editTask(task)">Editar</button>
-            <button class="delete-button" @click="deleteTask(task.id)">Eliminar</button>
+            <div class="task-actions">
+              <button @click="completeTask(task.id)">Completar</button>
+              <button @click="editTask(task)">Editar</button>
+              <button class="delete-button" @click="deleteTask(task.id)">Eliminar</button>
+            </div>
           </div>
         </div>
-      </div>
 
-      <div class="task-list">
-        <h2>Tareas Completadas</h2>
+        <div class="task-list">
+          <h2>Tareas Completadas</h2>
 
-        <div v-if="completedTasks.length === 0">No hay tareas completadas.</div>
+          <div v-if="completedTasks.length === 0">No hay tareas completadas.</div>
 
-        <div v-for="task in completedTasks" :key="task.id" class="task">
-          <div class="task-content">
-            <h3>{{ task.title }}</h3>
-            <p>{{ task.description }}</p>
-          </div>
+          <div v-for="task in completedTasks" :key="task.id" class="task">
+            <div class="task-content">
+              <h3>{{ task.title }}</h3>
+              <p>{{ task.description }}</p>
+            </div>
 
-          <div class="task-actions">
-            <button @click="uncompleteTask(task.id)">Descompletar</button>
-            <button @click="editTask(task)">Editar</button>
-            <button class="delete-button" @click="deleteTask(task.id)">Eliminar</button>
+            <div class="task-actions">
+              <button @click="uncompleteTask(task.id)">Descompletar</button>
+              <button @click="editTask(task)">Editar</button>
+              <button class="delete-button" @click="deleteTask(task.id)">Eliminar</button>
+            </div>
           </div>
         </div>
       </div>
@@ -66,20 +85,30 @@
 </template>
 
 <script>
-import Vue from 'vue';
-
 export default {
   data() {
     return {
       newTask: {
-        title: '',
-        description: '',
+        title: "",
+        description: "",
         completed: false,
         id: 1,
       },
       tasks: [],
       editMode: false,
       editedTask: null,
+      isLoggedIn: false,
+      showRegister: false,
+      registerForm: {
+        username: "",
+        password: "",
+      },
+      loginForm: {
+        username: "",
+        password: "",
+      },
+      users: [],
+      loggedInUser: "",
     };
   },
 
@@ -105,45 +134,132 @@ export default {
         this.tasks.push({ ...this.newTask, id: this.newTask.id++ });
       }
       this.resetForm();
+
+      localStorage.setItem('tasks', JSON.stringify(this.tasks));
     },
 
     deleteTask(taskId) {
-  this.tasks = this.tasks.filter((task) => task.id !== taskId);
-},
+      this.tasks = this.tasks.filter((task) => task.id !== taskId);
+      localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    },
 
-completeTask(taskId) {
-  const task = this.tasks.find((task) => task.id === taskId);
-  task.completed = true;
-},
+    completeTask(taskId) {
+      const task = this.tasks.find((task) => task.id === taskId);
+      task.completed = true;
+      localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    },
 
-uncompleteTask(taskId) {
-  const task = this.tasks.find((task) => task.id === taskId);
-  task.completed = false;
-},
+    uncompleteTask(taskId) {
+      const task = this.tasks.find((task) => task.id === taskId);
+      task.completed = false;
+      localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    },
 
-editTask(task) {
-  this.editMode = true;
-  this.editedTask = { ...task };
-  this.newTask = { ...task };
-},
+    editTask(task) {
+      this.editMode = true;
+      this.editedTask = { ...task };
+      this.newTask = { ...task };
+    },
 
-updateTask() {
-  const taskIndex = this.tasks.findIndex((task) => task.id === this.editedTask.id);
-  this.tasks.splice(taskIndex, 1, { ...this.newTask });
-  this.resetForm();
-},
+    updateTask() {
+      const taskIndex = this.tasks.findIndex((task) => task.id === this.editedTask.id);
+      this.tasks.splice(taskIndex, 1, { ...this.newTask });
+      this.resetForm();
 
-resetForm() {
-  this.newTask = {
-    title: '',
-    description: '',
-    completed: false,
-    id: this.newTask.id + 1,
-  };
-  this.editMode = false;
-  this.editedTask = null;
-},
-},
+      localStorage.setItem('tasks', JSON.stringify(this.tasks));
+    },
+
+    resetForm() {
+      this.newTask = {
+        title: '',
+        description: '',
+        completed: false,
+        id: this.newTask.id + 1,
+      };
+      this.editMode = false;
+      this.editedTask = null;
+    },
+
+    showRegisterForm() {
+      this.showRegister = true;
+    },
+
+    showLoginForm() {
+      this.showRegister = false;
+    },
+
+    registerUser() {
+      if (!this.registerForm.username || !this.registerForm.password) {
+        alert('Por favor ingrese un usuario y una contraseña válidos.');
+        return;
+      }
+
+      const user = this.users.find((user) => user.username === this.registerForm.username);
+      if (user) {
+        alert('El usuario ya está registrado.');
+        return;
+      }
+
+      this.users.push({
+        username: this.registerForm.username,
+        password: this.registerForm.password,
+      });
+
+      localStorage.setItem('users', JSON.stringify(this.users));
+
+      alert('Usuario registrado exitosamente.');
+
+      this.showLoginForm();
+      this.resetForm();
+    },
+
+    loginUser() {
+      if (!this.loginForm.username || !this.loginForm.password) {
+        alert("Por favor ingrese un usuario y una contraseña válidos.");
+        return;
+      }
+
+      const user = this.users.find(
+        (user) => user.username === this.loginForm.username
+      );
+      if (!user || user.password !== this.loginForm.password) {
+        alert("Credenciales inválidas. Por favor, intente nuevamente.");
+        return;
+      }
+
+      this.isLoggedIn = true;
+      this.loggedInUser = this.loginForm.username;
+      localStorage.setItem("loggedInUser", this.loginForm.username);
+
+      alert("Inicio de sesión exitoso.");
+    },
+
+    logout() {
+      this.isLoggedIn = false;
+      this.loggedInUser = "";
+      localStorage.removeItem("loggedInUser");
+
+      alert("Sesión cerrada exitosamente.");
+    },
+  },
+
+  created() {
+    const users = JSON.parse(localStorage.getItem('users'));
+    if (users) {
+      this.users = users;
+    }
+
+    const loggedInUser = localStorage.getItem("loggedInUser");
+    if (loggedInUser) {
+      this.isLoggedIn = true;
+      this.loggedInUser = loggedInUser;
+    }
+
+    const savedTasks = JSON.parse(localStorage.getItem('tasks'));
+    if (savedTasks) {
+      this.tasks = savedTasks;
+    }
+  },
 };
 </script>
 
@@ -296,4 +412,184 @@ form button:hover {
     transform: scale(1);
   }
 }
+
+/* Estilos para el formulario de inicio de sesión */
+.login-form {
+  max-width: 400px;
+  margin: 0 auto;
+  padding: 30px;
+  background-color: #fff;
+  border-radius: 8px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  animation: fadeIn 0.5s ease-in-out;
+}
+
+.login-form h2 {
+  text-align: center;
+  margin-bottom: 20px;
+  font-size: 2rem;
+  color: #333;
+}
+
+.login-form input[type="text"],
+.login-form input[type="password"] {
+  width: 100%;
+  padding: 12px;
+  margin-bottom: 20px;
+  border: none;
+  border-bottom: 2px solid #ccc;
+  background-color: transparent;
+  font-size: 1.2rem;
+  color: #333;
+  transition: border-color 0.3s ease-in-out;
+}
+
+.login-form input[type="text"]:focus,
+.login-form input[type="password"]:focus {
+  border-color: #45a049;
+}
+
+.login-form button {
+  width: 100%;
+  padding: 12px;
+  background-color: #45a049;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  font-size: 1.2rem;
+  cursor: pointer;
+  transition: background-color 0.3s ease-in-out;
+}
+
+.login-form button:hover {
+  background-color: #388e3c;
+}
+
+/* Estilos para el formulario de registro */
+.register-form {
+  max-width: 400px;
+  margin: 0 auto;
+  padding: 30px;
+  background-color: #fff;
+  border-radius: 8px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.2);
+  animation: fadeIn 0.5s ease-in-out;
+}
+
+.register-form h2 {
+  text-align: center;
+  margin-bottom: 20px;
+  font-size: 2rem;
+  color: #333;
+}
+
+.register-form input[type="text"],
+.register-form input[type="password"] {
+  width: 100%;
+  padding: 12px;
+  margin-bottom: 20px;
+  border: none;
+  border-bottom: 2px solid #ccc;
+  background-color: transparent;
+  font-size: 1.2rem;
+  color: #333;
+  transition: border-color 0.3s ease-in-out;
+}
+
+.register-form input[type="text"]:focus,
+.register-form input[type="password"]:focus {
+  border-color: #45a049;
+}
+
+.register-form button {
+  width: 100%;
+  padding: 12px;
+  background-color: #45a049;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  font-size: 1.2rem;
+  cursor: pointer;
+  transition: background-color 0.3s ease-in-out;
+}
+
+.register-form button:hover {
+  background-color: #388e3c;
+}
+
+/* Animación para mostrar los formularios */
+@keyframes fadeIn {
+  0% {
+    opacity: 0;
+    transform: translateY(-20px);
+  }
+  100% {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+
+.logout-button {
+  padding: 8px 16px;
+  background-color: #4caf50;
+  color: #fff;
+  border: none;
+  border-radius: 4px;
+  font-size: 1rem;
+  cursor: pointer;
+  transition: background-color 0.2s ease-in-out;
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+}
+
+.logout-button:hover {
+  background-color: #45a049;
+}
+
+.logout-button:focus {
+  outline: none;
+}
+
+.logout-button:active {
+  background-color: #3c893d;
+}
+
+.login-form button,
+.register-form button {
+  margin-top: 20px;
+}
+
+.username {
+  margin-right: 10px;
+  font-size: 18px;
+  color: white;
+  font-weight: bold;
+  text-transform: uppercase;
+  position: relative;
+  display: inline-block;
+  cursor: pointer;
+  transition: color 0.3s;
+  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+}
+
+.username:hover {
+  color: #000000;
+}
+
+.username::before {
+  content: "";
+  position: absolute;
+  bottom: -2px;
+  left: 0;
+  width: 100%;
+  height: 2px;
+  background-color: #000000;
+  transform: scaleX(0);
+  transform-origin: left center;
+  transition: transform 0.3s;
+}
+
+.username:hover::before {
+  transform: scaleX(1);
+}
+
 </style>
