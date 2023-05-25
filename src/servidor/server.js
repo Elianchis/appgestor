30cad9946c392a88f93e5c54e6c8d36b58cb1b15
Elianchis <eliantamayo6@gmail.com>
2023-05-25 17:21:59 -0500

    Actualización Servidor

--- src/servidor/server.js
+++ src/servidor/server.js
@@ -10,7 +10,7 @@ const connection = mysql.createConnection({
   host: 'localhost',
   port: 3306,
   user: 'root',
-  password: '', // Enter your MySQL password here if applicable
+  password: '',
   database: 'gestapp_db',
 });
 
